7ea909843b422029819fb9bfbb46444d014128b8
earl starks <earlstarks@rocketmail.com>
2022-12-01 22:49:28 -0600

    completed

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace BankEncapsulation
 {
-    public class Program
-    {
-        static void Main(string[] args)
+    public class Program {
+        public static void Main(string[] Args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine($" How much $ do you want to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine()!);
+
+            ba.deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+            Console.WriteLine($" Your current balance is {userBalance, 0:c}");
+
         }
+
+
+
+
+
+
     }
+    
+
+
+
+
+
+
 }
